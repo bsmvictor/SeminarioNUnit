6ba061a7b15ac62f89ea8d1935a2bfb6ef62e75f
bsmvictor <victor.boaventura@gec.inatel.br>
2024-11-09 15:18:18 -0300

    Criação da classe pilha a ser testada

---
+++ Pilha/Class1.cs
@@ -0,0 +1,32 @@
+﻿namespace Pilha
+{
+    public class Pilha<T>
+    {
+        private readonly List<T> elementos = new List<T>();
+
+        public void Push(T item)
+        {
+            elementos.Add(item);
+        }
+
+        public T Pop()
+        {
+            if (elementos.Count == 0)
+                throw new InvalidOperationException("A pilha está vazia.");
+
+            T item = elementos[^1];
+            elementos.RemoveAt(elementos.Count - 1);
+            return item;
+        }
+
+        public T Peek()
+        {
+            if (elementos.Count == 0)
+                throw new InvalidOperationException("A pilha está vazia.");
+
+            return elementos[^1];
+        }
+
+        public int Count => elementos.Count;
+    }
+}
