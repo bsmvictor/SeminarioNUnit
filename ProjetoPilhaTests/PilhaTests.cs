63fcdbbbd66d57b25dcb9f4bbb597fc2c820b265
Mysterion147 <joaopedromaciel0@gmail.com>
2024-11-11 10:19:37 -0300

    Mais testes

--- ProjetoPilhaTests/PilhaTests.cs
+++ ProjetoPilhaTests/PilhaTests.cs
@@ -87,4 +87,100 @@ public class PilhaTests
         pilha.Pop();
         Assert.That(pilha.Count, Is.EqualTo(0));
     }
+
+    /// Testa a adição de múltiplos itens do mesmo valor
+    [Test]
+    public void TestPushMultipleItemsWithSameValue()
+    {
+        pilha.Push(5);
+        pilha.Push(5);
+        pilha.Push(5);
+
+        Assert.That(pilha.Pop(), Is.EqualTo(5));
+        Assert.That(pilha.Pop(), Is.EqualTo(5));
+        Assert.That(pilha.Pop(), Is.EqualTo(5));
+        Assert.That(pilha.Count, Is.EqualTo(0));
+    }
+
+    /// Testa o comportamento da pilha com tipos de dados diferentes
+    [Test]
+    public void TestStackWithDifferentDataTypes()
+    {
+        var stringPilha = new Pilha<string>();
+        stringPilha.Push("Hello");
+        stringPilha.Push("World");
+
+        Assert.That(stringPilha.Pop(), Is.EqualTo("World"));
+        Assert.That(stringPilha.Pop(), Is.EqualTo("Hello"));
+    }
+
+    /// Testa se a pilha permanece consistente após várias operações intercaladas de Push e Pop
+    [Test]
+    public void TestStackConsistencyAfterMultiplePushAndPop()
+    {
+        pilha.Push(1);
+        pilha.Push(2);
+        pilha.Pop();      // Remove 2
+        pilha.Push(3);
+        pilha.Push(4);
+        pilha.Pop();      // Remove 4
+
+        Assert.That(pilha.Pop(), Is.EqualTo(3));
+        Assert.That(pilha.Pop(), Is.EqualTo(1));
+        Assert.That(pilha.Count, Is.EqualTo(0));
+    }
+
+    /// Testa a consistência do estado da pilha quando inserido e removido um grande número de itens
+    [Test]
+    public void TestStackWithLargeNumberOfItems()
+    {
+        for (int i = 1; i <= 1000; i++)
+        {
+            pilha.Push(i);
+        }
+
+        Assert.That(pilha.Count, Is.EqualTo(1000));
+
+        for (int i = 1000; i > 0; i--)
+        {
+            Assert.That(pilha.Pop(), Is.EqualTo(i));
+        }
+
+        Assert.That(pilha.Count, Is.EqualTo(0));
+    }
+
+    /// Testa o comportamento do Pop com itens complexos (objetos)
+    [Test]
+    public void TestStackWithComplexObjects()
+    {
+        var objectPilha = new Pilha<TestClass>();
+        var obj1 = new TestClass { Id = 1, Name = "Object 1" };
+        var obj2 = new TestClass { Id = 2, Name = "Object 2" };
+
+        objectPilha.Push(obj1);
+        objectPilha.Push(obj2);
+
+        Assert.That(objectPilha.Pop().Id, Is.EqualTo(2));
+        Assert.That(objectPilha.Pop().Id, Is.EqualTo(1));
+    }
+
+    /// Testa o comportamento do Peek com objetos sem removê-los
+    [Test]
+    public void TestPeekWithComplexObjects()
+    {
+        var objectPilha = new Pilha<TestClass>();
+        var obj = new TestClass { Id = 1, Name = "Object 1" };
+
+        objectPilha.Push(obj);
+        Assert.That(objectPilha.Peek().Id, Is.EqualTo(1));
+        Assert.That(objectPilha.Count, Is.EqualTo(1));  // Garante que o item não foi removido
+    }
+
+    /// Classe de teste auxiliar
+    public class TestClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
 }
